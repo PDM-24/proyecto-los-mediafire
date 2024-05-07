e25edd91d953db4654d5e67bca5cf21c57d8a497
carlosaleja <00116822@uca.edu.sv>
2024-05-06 22:46:03 -0600

    feat:Creando pantalla de bienvenida con terminos y condiciones

---
+++ app/src/main/java/com/ic/cinefile/screens/Bienvenida.kt
@@ -0,0 +1,150 @@
+package com.ic.cinefile.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+
+@Composable
+fun Content(navController: NavHostController) {
+
+    var aceptarTerminos by remember { mutableStateOf(false) }
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "¡Te damos la bienvenida a CineFile!",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center,
+            ),
+            modifier = Modifier.padding(start = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "Por favor, sigue las siguientes reglas: ",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Start,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(26.dp))
+
+        Text(
+            text = "Regla 1 \n Descripcion \n regla Regla 2 \n Descripcion regla  ",
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Start,
+                lineHeight = 24.sp
+            )
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable { aceptarTerminos = !aceptarTerminos }
+                .padding(start = 10.dp)
+        ) {
+            Checkbox(
+                checked = aceptarTerminos,
+                onCheckedChange = { aceptarTerminos = it },
+                modifier = Modifier.padding(end = 2.dp)
+            )
+
+            Text(
+                text = "He leído y acepto los términos y condiciones de uso.",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Start,
+                )
+            )
+        }
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .width(300.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.White,
+                contentColor = Color.Black
+            ),
+        ) {
+            Text(
+                text = "Acepto",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                )
+            )
+        }
+
+
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewBienvenidaScreen() {
+    val navController = rememberNavController()
+    Content(navController)
+}
