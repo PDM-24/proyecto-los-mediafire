7220a36f7679658074b3a4f861b14ebb7362f250
JH074 <00162622@uca.edu.sv>
2024-06-17 17:44:00 -0600

    feat: comentarios y respuestas peli

---
+++ app/src/main/java/com/ic/cinefile/components/seccComentarios/respuestas.kt
@@ -0,0 +1,53 @@
+package com.ic.cinefile.components.seccComentarios
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun respuestas(idComentario: String?, imagePainter: Painter?, description: String?) {
+    //val comment = "Comentario $idComentario"
+
+    //El comentario que se responde
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (imagePainter != null) {
+                Image(
+                    painter = imagePainter,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .clip(CircleShape)
+                )
+            }
+            if (description != null) {
+                Text(
+                    text = description,
+                    modifier = Modifier.padding(start = 10.dp)
+                )
+            }
+
+        }
+    }
+}
